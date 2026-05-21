5a7a4155cb8ccdf7fb17f112e37e1e4e84a7aa49
shane <shanerobihemed@gmail.com>
2026-05-21 13:11:33 +0800

    self checkout

--- exercise_files/01_04/hello.py
+++ exercise_files/01_04/hello.py
@@ -1,3 +1,9 @@
 
 # Print "Hello, World!" to the terminal
-print('Hello, World!')
+
+user_input = input("Please tell me if you are okay. ")
+
+if "yes" in user_input or "yea" in user_input:
+    print("thats nice to hear")
+elif "no" in user_input or "not" in user_input:
+    print("why not?")

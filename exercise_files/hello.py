891a1832fe1cb332de606020bccc6f28191d40b1
shane <shanerobihemed@gmail.com>
2026-05-23 11:37:28 +0800

    testing again

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -1,3 +1,6 @@
 
 # Print "hello, world!" to the terminal
-print('Hello, World!')
+if "Yes" in input("is your name world?"):
+    print('Hello, World!')
+else:
+    print("what the hell ever are you?")

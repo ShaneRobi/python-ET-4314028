16ef6be09e7bf3d9f8f3f3abf0d80a9fe5b0f436
shane <shanerobihemed@gmail.com>
2026-05-28 09:43:44 +0800

    bogus

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -2,5 +2,8 @@
 # Print "hello, world!" to the terminal
 if "Yes" in input("is your name world?"):
     print('Hello, World!')
+    
 else:
     print("what the hell ever are you?")
+elif: "No" in input("is your name world?"):
+    print("what the hell ever are you?")    

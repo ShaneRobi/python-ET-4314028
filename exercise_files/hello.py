5400c29ee099fbcdfa47fc3584f2a482e41b3d9e
shane <shanerobihemed@gmail.com>
2026-05-30 10:46:32 +0800

    na

--- exercise_files/hello.py
+++ exercise_files/hello.py
@@ -7,3 +7,6 @@ else:
     print("what the hell ever are you?")
 elif: "No" in input("is your name world?"):
     print("what the hell ever are you?")    
+
+
+    prinyt("what the hell ever are you?")\
